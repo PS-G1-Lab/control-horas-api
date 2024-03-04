3fcbadcdbd229750b8629e2c0f23c6e51bec570b
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-04 10:28:54 +0000

    Add review and user schemas

---
+++ src/schemas/user.js
@@ -0,0 +1,15 @@
+import z from 'zod'
+
+const userSchema = z.object({
+  userName: z.string().min(3).max(255),
+  email: z.string().email(),
+  password: z.string().min(8).max(255).optional()
+})
+
+export function validateUser (input) {
+  return userSchema.safeParse(input)
+}
+
+export function validatePartialUser (input) {
+  return userSchema.partial().safeParse(input)
+}
