9f5442436ab01db49e5087743e01a9f1000cd717
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-06 14:25:11 +0000

    Add linters config files (ESLint, Prettier, Husky)

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+	env: {
+		es2022: true,
+		node: true,
+		browser: true,
+	},
+	extends: ["@antfu", "eslint-config-prettier"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"eol-last": "off",
+		"jsx-quotes": ["warn", "prefer-double"],
+		"quotes": ["warn", "double"],
+		"semi": ["warn", "never"],
+		"@stylistic/js/no-tabs": "off",
+		"@stylistic/ts/indent": "off",
+		"no-constant-binary-expression": "warn",
+		"no-debugger": "warn",
+		"no-extend-native": "off",
+		"no-trailing-spaces": "warn",
+		"space-before-function-paren": "off",
+		"antfu/if-newline": "off",
+		"antfu/top-level-function": "off",
+		"@stylistic/js/operator-linebreak": "off",
+		"@stylistic/ts/brace-style": "off",
+		"@stylistic/js/multiline-ternary": "off",
+		"n/prefer-global/process": "off",
+		"@stylistic/js/no-mixed-spaces-and-tabs": "off",
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": "off",
+		"unused-imports/no-unused-imports": "warn",
+		"object-curly-newline": [
+			"warn",
+			{
+				consistent: true,
+				multiline: true,
+			},
+		],
+		"object-curly-spacing": ["warn", "always"],
+		"array-element-newline": ["warn", "consistent"],
+		"array-bracket-newline": ["warn", "consistent"],
+	}
+}
