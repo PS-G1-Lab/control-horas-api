b07dc109d7b90ce7f44c607299309c31f0441172
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-03-19 22:07:12 +0000

    Refactor user and password validation schemas

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -2,27 +2,45 @@ import z from "zod"
 
 const userSchema = z.object({
 	userName: z
-		.string({
-			invalid_type_error: "Username must be a string just like this one",
+		.string()
+		.min(3, {
+			message: "Nombre demasiado corto (mínimo 3 caracteres)",
 		})
-		.min(3)
-		.max(255),
-	email: z.string().email(),
-	password: z
-		.string({
-			invalid_type_error: "Password must be a string just like this one",
-		})
-		.min(8, {
-			message:
-				"Password must be at least 8 characters long and contain at least one number and one letter",
-		})
-		.max(255, {
-			message:
-				"Password must be at most 255 characters long and contain at least one number and one letter",
-		})
-		.optional(),
+		.max(100, {
+			message: "Nombre demasiado largo (máximo 100 caracteres)",
+		}),
+	email: z.string().email({
+		message: "Debe ser un email válido",
+	}),
 	role: z.enum(["0", "1"]).optional(),
 })
+
+const passwordForm = z
+	.object({
+		password: z
+			.string()
+			.min(8, {
+				message: "Contraseña demasiado corta (mínimo 8 caracteres)",
+			})
+			.max(30, {
+				message: "Contraseña demasiado larga (máximo 30 caracteres)",
+			})
+			.regex(/.*\d.*/, { message: "Debe haber al menos un número" })
+			.regex(/.*[!@#$&?*_].*/, { message: "Debe haber al menos un carácter especial" })
+			.regex(/.*[a-z].*/, { message: "Debe haber al menos una letra minúscula" })
+			.regex(/.*[A-Z].*/, { message: "Debe haber al menos una letra mayúscula" })
+			.refine((value) => !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value), {
+				message: "No deben haber patrones de email",
+			})
+			.refine((value) => !/^[a-zA-Z0-9]+$/.test(value), {
+				message: "No deben haber patrones de letras y/o números",
+			}),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Las contraseñas no coinciden",
+		path: ["confirmPassword"],
+	})
 
 export function validateUser(input) {
 	return userSchema.safeParse(input)
@@ -30,4 +48,8 @@ export function validateUser(input) {
 
 export function validatePartialUser(input) {
 	return userSchema.partial().safeParse(input)
+}
+
+export function validatePasswordForm(input) {
+	return passwordForm.safeParse(input)
 }
