8c9e074c14ecfe2174e22d85dcd4f75a4becea73
XxMARRExX <xx_marre_xx@hotmail.com>
2024-04-21 17:41:43 +0100

    Changes to prove send register mail in PROD

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,6 @@
-// import { UserModel } from "../models/turso/user.js"
-import { UserModel } from "../models/postgresql/user.js"
+import { UserModel } from "../models/turso/user.js"
+
+// import { UserModel } from "../models/postgresql/user.js"
 import { validatePasswordForm, validateUser } from "../schemas/user.js"
 import { sendMailVerification } from "../services/mail/sendMailRegister.js"
 
