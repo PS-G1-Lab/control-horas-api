38c843eaa6f33f8caf02cf59f0af55d95933a220
Kilamper <kilamper@gmail.com>
2024-05-10 19:51:21 +0100

    Update PostgreSQL class model and createClass method in ClassModel

--- src/models/postgresql/class.js
+++ src/models/postgresql/class.js
@@ -1,37 +1,49 @@
 import dotenv from "dotenv"
-import { Client } from "pg"
+import pg from "pg"
 
 dotenv.config({ path: "./.env" })
 
+const { Client } = pg
+
 const client = new Client({
-	connectionString: process.env.POSTGRES_URL,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+	connectionString: process.env.DB_CONNECTION_STRING,
 })
 
 client.connect()
 
 export class ClassModel {
 	static async init() {
-		const createClassesTable = `
-      CREATE TABLE IF NOT EXISTS classes (
-        class_id SERIAL PRIMARY KEY,
-        user_id INTEGER NOT NULL,
-        title TEXT NOT NULL,
-        subject TEXT NOT NULL,
-        students INTEGER DEFAULT 0,
-        start_at TIMESTAMP NOT NULL,
-        end TIMESTAMP NOT NULL,
-        date TIMESTAMP NOT NULL,
-        description TEXT DEFAULT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-      );
-    `
+		const createClassesTable = await client
+			.query(
+				`
+        CREATE TABLE IF NOT EXISTS classes (
+          class_id SERIAL PRIMARY KEY,
+          user_id INTEGER NOT NULL,
+          title TEXT NOT NULL,
+          subject TEXT NOT NULL,
+          students INTEGER DEFAULT 0,
+          start_at TIMESTAMP NOT NULL,
+          end TIMESTAMP NOT NULL,
+          date TIMESTAMP NOT NULL,
+          description TEXT DEFAULT NULL,
+          FOREIGN KEY (user_id) REFERENCES users(user_id)
+        );
+        `
+			)
+			.catch((error) => {
+				return { error }
+			})
 
-		try {
-			await client.query(createClassesTable)
-			return { message: "Tabla 'classes' creada" }
-		} catch (error) {
-			return { error }
+		if (createClassesTable.error) {
+			return { error: "Error al crear tabla 'classes'" }
 		}
+
+		return { message: "Tabla 'classes' creada" }
 	}
 
 	static async createClass({ input }) {
@@ -43,18 +55,22 @@ export class ClassModel {
 			.normalize("NFD")
 			.replace(/[\u0300-\u036F]/g, "")
 
-		try {
-			await client.query(
+		const newUser = await client
+			.query(
 				`
         INSERT INTO classes (user_id, title, subject, start_at, end, date, description)
         VALUES ($1, $2, $3, $4, $5, $6, $7)
         `,
 				[userId, title, subjectName, startAt, end, date, description]
 			)
-			return { message: "Clase creada" }
-		} catch (error) {
-			console.log(error)
+			.catch((error) => {
+				return { error }
+			})
+
+		if (newUser.error) {
 			return { error: "Error al crear clase" }
 		}
+
+		return { message: "Clase creada" }
 	}
 }
