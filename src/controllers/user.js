80b010dd6b61387faa95424cb946ddc2b411a14d
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-05-10 11:09:38 +0100

    refactor: update user.js model to fix issue with retrieving user ID from database

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -66,7 +66,7 @@ export class UserController {
 			return res.status(404).json({ error: userExists.error })
 		}
 
-		const input = { password, userId: userExists.userId }
+		const input = { password, userId: userExists.userId.user_id }
 
 		return res.status(200).json({ input })
 
