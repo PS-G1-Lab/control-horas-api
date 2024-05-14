540f9b9b11b5010f48ac34e50e4eba75e6aea6c3
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-05-14 13:58:24 +0100

    feat: Add deleteClass endpoint to classRouter

--- src/routes/class.js
+++ src/routes/class.js
@@ -5,3 +5,4 @@ import { ClassController } from "../controllers/class.js"
 export const classRouter = Router()
 
 classRouter.post("/", ClassController.createClass)
+classRouter.post("/delete/:classId", ClassController.deleteClass)
